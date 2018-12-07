302e1f898691db2901f71f801ad72d97ce4badb7
Shingi Mutandwa <shingi.mutandwa@gmail.com>
2018-12-07 19:52:01 +1000

    added tracing

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/shingi/trace"
 )
 
 // templ represents a single template
@@ -27,6 +30,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "Te address of the application.")
 	flag.Parse()
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
