2d17c98385b71effa81388db665fa3cf04060a37
Shingi Mutandwa <shingi.mutandwa@gmail.com>
2018-12-07 22:15:02 +1000

    login page and dynamic path-matching mechanism

--- auth.go
+++ auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 // implements and wraps http.Handler interface
 type authHandler struct {
@@ -32,4 +37,24 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // simply creates authHandler that wraps any other http.Handler
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// dynamic path-matching mechanism
+// loginHandler handles the third-party login process.
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+    if len(segs) < 3 {
+        http.Error(w, "Unsupported url", http.StatusBadRequest)
+        return
+    }
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
--- main.go
+++ main.go
@@ -32,6 +32,8 @@ func main() {
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+    http.Handle("/login", &templateHandler{filename: "login.html"})
+    http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// get the room going
